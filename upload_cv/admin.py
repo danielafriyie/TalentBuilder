6cd538ae6fd233d3c0afd4bf89a49dfdd14cbff0
danielafriyie <55259794+grandpaSenior@users.noreply.github.com>
2020-02-18 21:32:02 +0000

    made some changes

--- upload_cv/admin.py
+++ upload_cv/admin.py
@@ -3,9 +3,11 @@ from . import models
 
 
 class UploadCVAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'phone', 'cv_format', 'career_type', 'agree_to_terms', 'revised_cv')
+    list_display = (
+        'id', 'name', 'email', 'phone', 'cv_format', 'career_type', 'agree_to_terms', 'cv_upload', 'revised_cv'
+    )
     list_display_links = ('id', 'name', 'email')
-    list_editable = ('revised_cv',)
+    list_editable = ('cv_upload', 'revised_cv',)
     list_filter = ('cv_format', 'date', 'agree_to_terms')
     search_fields = ('name', 'email', 'cv_format', 'date', 'phone')
     list_per_page = 25
@@ -21,6 +23,7 @@ class EmailActionAdmin(admin.ModelAdmin):
     search_fields = ('id', 'client_id', 'date')
     list_per_page = 25
 
+
 admin.site.register(models.EmailAction, EmailActionAdmin)
 
 
@@ -30,5 +33,6 @@ class MailMessageAdmin(admin.ModelAdmin):
     list_filter = ('date',)
     search_fields = ('id', 'host_user', 'date')
     list_per_page = 25
+
 
 admin.site.register(models.UploadCVMailMessage, MailMessageAdmin)
